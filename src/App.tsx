27cd345eea84e1bacb0a8b0fae52987387c88cb7
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-09-15 08:24:36 -0400

    add the dockerfile and initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+
+import { Grid } from "@mui/material";
+import PrimarySearchAppBar from "./components/nav";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid container lg={12} rowGap={2}>
+        <Grid container>
+          <PrimarySearchAppBar />
+        </Grid>
+        <Grid
+          container
+          justifyContent="space-around"
+          alignItems="center"
+          sx={{
+            bgcolor: "aqua",
+            minHeight: "92vh",
+            borderRadius: "15px 15px 0px 0px"
+          }}
+        >
+          <Grid
+            container
+            lg={7.5}
+            sx={{ bgcolor: "orangered", height: "95%", borderRadius: "15px" }}
+          ></Grid>
+          <Grid
+            container
+            lg={4}
+            sx={{ bgcolor: "blue", height: "95%", borderRadius: "15px" }}
+          ></Grid>
+        </Grid>
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
