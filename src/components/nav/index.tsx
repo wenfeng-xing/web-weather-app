b4f3731c9b5773858ad5d25a35a9fa0a7dada991
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-09-15 13:24:25 -0400

    add the weather icon and the address component

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -9,12 +9,14 @@ import InputBase from "@mui/material/InputBase";
 import Badge from "@mui/material/Badge";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import SearchIcon from "@mui/icons-material/Search";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
+
+import Address from "./address";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -166,16 +168,9 @@ export default function PrimarySearchAppBar() {
             aria-label="open drawer"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <WbSunnyIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ display: { xs: "none", sm: "block" } }}
-          >
-            Montreal
-          </Typography>
+          <Address />
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
