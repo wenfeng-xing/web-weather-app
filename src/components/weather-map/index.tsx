07c1104f4ff3c4cf9650e732b9121e452b4f6fc3
Wenfeng Xing <wenfeng.xing@weareadaptivee.com>
2022-09-16 11:48:56 -0400

    fix: can not display icon

--- src/components/weather-map/index.tsx
+++ src/components/weather-map/index.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react'
+import L from 'leaflet';
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import { Paper, Grid } from "@mui/material";
 import 'leaflet/dist/leaflet.css';
+
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
 const WeatherMap: React.FunctionComponent = () => {
   return (
     <Paper style={{height: "100%"}}>
